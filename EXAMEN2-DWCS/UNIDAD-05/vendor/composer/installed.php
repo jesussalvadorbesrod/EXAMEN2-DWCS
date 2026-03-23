25d7d4aeb3c2e3b685e5894519040dec1fafbd89
David García Cancio <davidgaca@edu.xunta.gal>
2026-03-23 17:51:48 +0100

    D# Fin corrección PRACTICO-05

---
+++ EXAMEN2-DWCS/UNIDAD-05/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'jesussalvadorbesrod/examen-dwcs',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '075f6475e994d08df591fb835e593658e00ce941',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'eftec/bladeone' => array(
+            'pretty_version' => '4.19.1',
+            'version' => '4.19.1.0',
+            'reference' => 'a22f6471c01c26fea5f6a0d0aca9b5674fc999be',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../eftec/bladeone',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'jesussalvadorbesrod/examen-dwcs' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '075f6475e994d08df591fb835e593658e00ce941',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
